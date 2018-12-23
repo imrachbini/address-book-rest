00b43121bf15fe884b64d6f519de5c68d664f9e5
Imam Maulana Akbar Rachbini <imrachbini@gmail.com>
2018-12-24 00:11:52 +0700

    register urls

---
+++ app_contacts/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url, include
+from app_contacts.views import (
+    ContactList,
+    ContactDetail,
+)
+
+urlpatterns = [
+    url(r'^$',
+        ContactList.as_view(),
+        name="contact-list"),
+    url(r'^(?P<pk>[0-9]+)/$',
+        ContactDetail.as_view(),
+        name="contact-detail"),
+]
