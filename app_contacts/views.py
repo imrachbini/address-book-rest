928a277d4004e585beb550fbec422429a217f49d
Imam Maulana Akbar Rachbini <imrachbini@gmail.com>
2018-12-24 00:11:45 +0700

    create add edit delete functionality

--- app_contacts/views.py
+++ app_contacts/views.py
@@ -1,6 +1,29 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from rest_framework.generics import (
+    ListAPIView, RetrieveAPIView, ListCreateAPIView, CreateAPIView,
+    RetrieveUpdateDestroyAPIView
+)
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from app_contacts.models import Contact
+from app_contacts.serializers import ContactSerializer
 
-from django.shortcuts import render
 
-# Create your views here.
+class ContactList(ListCreateAPIView):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
+
+
+class ContactDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
+
+    def get(self, request, pk, format=None):
+        try:
+            contact_detail = Contact.objects.get(id=pk)
+            serializer = self.get_serializer(contact_detail)
+            return Response(serializer.data)
+        except Contact.DoesNotExist:
+            body = {
+                'message': 'Contact not found',
+            }
+            return Response(body, 404)
