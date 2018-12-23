33f7e7ca1037381e520272798b8011ce58a91b34
Imam Maulana Akbar Rachbini <imrachbini@gmail.com>
2018-12-24 00:10:51 +0700

    create contact model

--- app_contacts/models.py
+++ app_contacts/models.py
@@ -1,6 +1,13 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+class Contact(models.Model):
+    full_name = models.CharField(max_length=128, null=False)
+    home_no = models.CharField(max_length=20, null=True)
+    mobile_no = models.CharField(max_length=20, null=True)
+    address = models.TextField(max_length=500, null=True)
+
+    def __str__(self):
+        return self.full_name
+
+    class Meta:
+        ordering = ['full_name']
