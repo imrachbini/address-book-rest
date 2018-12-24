3143e9a9af7c206c44a030d174a696f0d427b497
Imam Maulana Akbar Rachbini <imrachbini@gmail.com>
2018-12-24 11:25:38 +0700

    prod mode

--- contacts/settings.py
+++ contacts/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'v3zessx0skqht2glu%mg)4*y$k+96e6i0^cl2tdt)%z_abopfm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
